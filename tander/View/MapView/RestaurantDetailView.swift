b484bdaf3896c3ab843cf8837eebb80e82d774c0
Jirayut Pathaveesrisutha <jirayut_time@hotmail.com>
2020-04-05 17:08:33 +0700

    create Detailview for restaurant

--- tander/View/MapView/RestaurantDetailView.swift
+++ tander/View/MapView/RestaurantDetailView.swift
@@ -10,25 +10,58 @@ import SwiftUI
 
 struct RestaurantDetailView: View {
     
-    @ObservedObject var mapVM = MapViewModel()
-
-
+    var restaurant: Restaurant
+    
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Image("defaultImg")
-                .frame(width: 300.0, height: 300.0)
-            VStack{
-                Text("Title")
-                    .font(.title)
-                Text("subtitle")
-                    .font(.subheadline)
+                .resizable()
+                .scaledToFit()
+                .padding(.bottom)
+            HStack{
+                VStack(alignment: .leading){
+                    Text(restaurant.name!)
+                        .font(.title)
+                    Text(restaurant.categories![0])
+                        .font(.body)
+                }.padding(.leading, 25)
+                Spacer()
             }
+            
+            Divider()
+            
+            //will fix to hstack because if 2 line the second line will start where location start
+            Text("Location : \(restaurant.address!)").padding(.leading, 25)
+            
+            HStack {
+                Spacer()
+                Button(action:{}){
+                    Text("Create")
+                }
+                .padding(.all , 5)
+                .background(Color.gray)
+                .foregroundColor(Color.white)
+                
+                Button(action:{}){
+                    Text("Join")
+                }
+                
+                .padding(.all , 5)
+                .background(Color.gray)
+                .foregroundColor(Color.white)
+                
+            }.padding()
+            
+            Spacer()
+            
         }
+         .navigationBarTitle(Text(restaurant.name!), displayMode: .inline)
     }
 }
 
 struct RestaurantDetailView_Previews: PreviewProvider {
+    
     static var previews: some View {
-        RestaurantDetailView(mapVM: MapViewModel())
+        RestaurantDetailView(restaurant:  Restaurant(_id: "test", categories: ["thai"], placeID: "test", name: "example Annotation", url: "", startPrice: 199, address: "Kasetsart", position: Restaurant.cod(lat: 13.8476, lon: 100.5696), isPartner: false, __v: 0))
     }
 }
